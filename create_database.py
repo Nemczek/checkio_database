57caa066a064f434279d32a82c24c8b1cdbd748e
Nemczek <p.baran890@gmail.com>
2023-10-25 19:15:49 +0200

    writing database name tp json file

--- create_database.py
+++ create_database.py
@@ -3,6 +3,8 @@ import sqlite3 as sq
 import pandas as pd
 import fetch_data
 import get_slug
+import json
+import os
 
 def create_sql_engine(name_of_database_file: str) -> db.engine.base.Engine:
     """
@@ -26,37 +28,56 @@ def connect_to_database(db_name: str) -> sq.Connection:
         print(f'Failed to connect to database {error}') # Add log entry later 
 
 
-DB_NAME = 'test_database'
-token = get_slug.get_token('checkio_token.txt')
-slug = get_slug.get_class_slug(token)
+def write_db_name_to_json(db_name: str) -> None:
+    """
+    Function writes database name do json file
+    """
+    name = {
+        "name": db_name
+    }
 
-data_tasks = fetch_data.fetch_task_data(slug, token)
-data_entries = fetch_data.fetch_entry_data(slug, token)
+    if not os.path.exists(f'{db_name}.json'):
+        data = json.dumps(name, indent=4)
+        with open(f'{db_name}.json', 'w') as file:
+            file.write(data)
+    else:
+        print("File alredy exists")
 
-engine = create_sql_engine(DB_NAME)
 
-# Creating tables in database. If table already exists, it appends new records.
-data_tasks.to_sql('tasks', con=engine, index=False, if_exists='append', schema=None)
-data_entries.to_sql('entries', con=engine, index=False, if_exists='append', schema=None)
 
-# Simple testing
-connection = connect_to_database(DB_NAME)
-cursor = connection.cursor()
+if __name__ == '__main__':
+    DB_NAME = 'test_database'
+    write_db_name_to_json(DB_NAME)
+    token = get_slug.get_token('checkio_token.txt')
+    slug = get_slug.get_class_slug(token)
 
-query = "SELECT * FROM tasks"
-result = cursor.execute(query)
-rows = result.fetchall()
+    data_tasks = fetch_data.fetch_task_data(slug, token)
+    data_entries = fetch_data.fetch_entry_data(slug, token)
 
-print(pd.DataFrame(rows, columns=map(lambda x: x[0], result.description)))
+    engine = create_sql_engine(DB_NAME)
 
-query2 = "SELECT * FROM entries"
-result2 = cursor.execute(query2)
-rows2 = result.fetchall()
+    # Creating tables in database. If table already exists, it appends new records.
+    data_tasks.to_sql('tasks', con=engine, index=False, if_exists='append', schema=None)
+    data_entries.to_sql('entries', con=engine, index=False, if_exists='append', schema=None)
 
-print(pd.DataFrame(rows2, columns=map(lambda x2: x2[0], result2.description)))
+    # Simple testing
+    connection = connect_to_database(DB_NAME)
+    cursor = connection.cursor()
 
-cursor.close()
-connection.close()
+    query = "SELECT * FROM tasks"
+    result = cursor.execute(query)
+    rows = result.fetchall()
+
+    print(pd.DataFrame(rows, columns=map(lambda x: x[0], result.description)))
+
+    query2 = "SELECT * FROM entries"
+    result2 = cursor.execute(query2)
+    rows2 = result.fetchall()
+
+    print(pd.DataFrame(rows2, columns=map(lambda x2: x2[0], result2.description)))
+
+    cursor.close()
+    connection.close()
 
 
     
